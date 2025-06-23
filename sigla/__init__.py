0457baee18546c7d6b359749250a973f030e0ac8
Amir Tlinov <89709267+iMAGRAY@users.noreply.github.com>
2025-06-23 19:34:06 +0300

    Expose summarizer cache control

---
+++ sigla/__init__.py
@@ -0,0 +1,35 @@
+from .core import (
+    CapsuleStore,
+    merge_capsules,
+    compress_capsules,
+    sanitize_text,
+    capsulate_text,
+    clear_summarizer_cache,
+)
+from .dsl import INTENT, RETRIEVE, MERGE, INJECT, EXPAND
+from .graph import random_walk_links, to_dot
+from .log import start as start_log, log as log_event
+
+try:
+    from .server import app as SiglaApp
+except Exception:  # pragma: no cover - optional dependency
+    SiglaApp = None
+
+__all__ = [
+    "CapsuleStore",
+    "merge_capsules",
+    "compress_capsules",
+    "sanitize_text",
+    "capsulate_text",
+    "SiglaApp",
+    "INTENT",
+    "RETRIEVE",
+    "MERGE",
+    "INJECT",
+    "EXPAND",
+    "random_walk_links",
+    "to_dot",
+    "start_log",
+    "log_event",
+    "clear_summarizer_cache",
+]
