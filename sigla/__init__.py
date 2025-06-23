a761139ca4e3529b31672bf7479f59fd11ff15c5
Amir Tlinov <89709267+iMAGRAY@users.noreply.github.com>
2025-06-23 22:37:05 +0300

    Add /version API endpoint

---
+++ sigla/__init__.py
@@ -0,0 +1,42 @@
+__version__ = "0.1.0"
+
+from .core import (
+    CapsuleStore,
+    merge_capsules,
+    compress_capsules,
+    text_similarity,
+    sanitize_text,
+    capsulate_text,
+    clear_summarizer_cache,
+)
+from .dsl import INTENT, RETRIEVE, MERGE, INJECT, EXPAND
+from .graph import random_walk_links, to_dot
+from .log import start as start_log, log as log_event
+from .llm import LocalLLM
+
+try:
+    from .server import app as SiglaApp
+except Exception:  # pragma: no cover - optional dependency
+    SiglaApp = None
+
+__all__ = [
+    "CapsuleStore",
+    "merge_capsules",
+    "compress_capsules",
+    "text_similarity",
+    "sanitize_text",
+    "capsulate_text",
+    "SiglaApp",
+    "INTENT",
+    "RETRIEVE",
+    "MERGE",
+    "INJECT",
+    "EXPAND",
+    "random_walk_links",
+    "to_dot",
+    "start_log",
+    "log_event",
+    "clear_summarizer_cache",
+    "LocalLLM",
+    "__version__",
+]
